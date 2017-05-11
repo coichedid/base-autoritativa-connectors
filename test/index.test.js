d78c046cb66099595f680c824f93d780ed776eef
Clovis Chedid <coichedid@gmail.com>
2017-05-11 16:34:30 -0300

    Initial

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+describe('MapaInformacaoConnector', () => {
+  it('should get all sistemas', () => {
+
+  });
+  it('should get only one sistema', () => {
+
+  });
+  it('should get all users of a sistema', () => {
+
+  });
+  it('should get all tables of a list of users', () => {
+    
+  });
+
+});
